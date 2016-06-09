b72ed219af0852e081b97a5f9e7a39d944bfae3c
TejaswiR <tejaswi.ramisetty@gmail.com>
2016-06-09 20:06:24 +0530

    displayed info on main page and made changes in the view

--- scripts/app.js
+++ scripts/app.js
@@ -10,6 +10,12 @@ $routeProvider
     controllerAs:'mc'
   })
 
+  {
+    templateUrl: 'pages/percent.html',
+    controller: 'PercentController',
+    controllerAs:'pc'
+  })
+
 });
 
 
@@ -21,4 +27,9 @@ function($resource){
    console.log(vm.firstResponse);
 }]);
 
-
+angularApp.controller("PercentController",['$resorce',
+  function($resource){
+    var vm = this;
+    var second =$resource('http://api.census.gov/data/timeseries/idb/1year?get=AREA_KM2,NAME,AGE,POP&FIPS=IN&time=2014&SEX=2');
+    function percentage()
+  }])
